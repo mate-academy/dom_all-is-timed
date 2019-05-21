41ac9e6269840cfa9e618ab60ca107b04b9bc954
VladimirTi <tytmes@mail.ru>
2019-05-21 09:55:57 +0300

    button added

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-let count = localStorage.getItem('time') ? localStorage.getItem('time') : 0;
+let count = localStorage.getItem('time');
+if(!count) {
+  count = 0;
+}
+
 const htmlCount = document.createElement('p')
-let timeout = setInterval(() => {
+
+setInterval(() => {
   count++;
-  localStorage.setItem('time', `${count}`);
-  htmlCount.innerHTML = `General time at this site is: ${Math.floor(count / 60)}:${count % 60}`;
-  document.querySelector('body').append(htmlCount);
-  console.log(count);
+  let seconds = count % 60;
+  if(seconds < 10) {
+    seconds = '0' + seconds;
+  }
+  htmlCount.innerHTML = `General time at this site is: ${Math.floor(count / 60)}:${seconds}`;
+  document.querySelector('body').prepend(htmlCount);
 }, 1000);
+
+window.addEventListener('beforeunload', () => {
+  localStorage.setItem('time', `${count}`);
+});
+
+document.querySelector('button').addEventListener('click', () => {
+  localStorage.setItem('time', 0);
+  count = 0;
+});
